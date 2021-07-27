a5f1c76404af5d9391282a08101246f7b32cd815
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2021-07-27 09:42:50 +0300

    создал разметку с помощью шаблонизатора

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles.css';
+import templateFunction from './templates/menu-cards.hbs';
+import menu from './menu.json';
+
+const ulMenu = document.querySelector('.js-menu');
+const markup = templateFunction(menu);
+// console.log(templateFunction);
+console.log(markup);
+console.log(menu[0]);
+
+ulMenu.insertAdjacentHTML('beforeend', markup);
